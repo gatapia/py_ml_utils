fad29de8caf7a82866a6606f55af4349aa05e291
Admin <gatapia@ubuntu.(none)>
2014-05-25 14:43:14 +1000

    feature selector now uses column names if specified

--- feature_sel.py
+++ feature_sel.py
@@ -1,11 +1,14 @@
 from misc import *
 import time
+import operator
 
-def feature_select(X_train, y_train, model, n_samples=3500, n_iter=3, tol=0.00001):
+def feature_select(X_train, y_train, model, n_samples=3500, n_iter=3, 
+    tol=0.00001, column_names=[]):
   score_hist = []
   good_features = set([])
   last_score = 0
   n_features = X_train.shape[1]
+  column_names = column_names if len(column_names) == n_features else range(n_features)
   # FEATURE SELECTION
   while len(score_hist) < 2 or (score_hist[-1][0] - score_hist[-2][0] > tol):
     scores = []  
@@ -31,11 +34,12 @@ def feature_select(X_train, y_train, model, n_samples=3500, n_iter=3, tol=0.0000
     last_score = best[0]
     good_features.add(best[1])
     score_hist.append(best)
+    sorted_good = sorted(list(good_features))
     print "Iteration Took: %.2fm - Current Features: %s" % \
-      ((time.clock() - t0)/60, sorted(list(good_features)))
+      ((time.clock() - t0)/60, operator.itemgetter(sorted_good)(column_names))
 
   # Remove last added feature from good_features
   good_features.remove(score_hist[-1][1])
   good_features = sorted(list(good_features))
-  print "Selected features %s" % good_features
+  print "Selected features %s - names %s" % (good_features, operator.itemgetter(good_features)(column_names))
   return good_features
