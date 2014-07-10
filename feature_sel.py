f69e842eba1ee8771715bd6c72ab222620330419
Guido Tapia <guido@tapia.com.au>
2014-07-10 16:04:42 +1000

    Minor enhacements

--- feature_sel.py
+++ feature_sel.py
@@ -3,19 +3,19 @@ import time
 import operator
 
 def feature_select(X_train, y_train, model, n_samples=3500, n_iter=3, 
-    tol=0.00001, column_names=[], scoring=None):
+    tol=0.000001, column_names=[], scoring=None):
   score_hist = []
-  good_features = set([])
+  good_features = []
+  good_scores = []
   last_score = 0
   n_features = X_train.shape[1]
   column_names = column_names if len(column_names) == n_features else range(n_features)
-  # FEATURE SELECTION
   while len(score_hist) < 2 or (score_hist[-1][0] - score_hist[-2][0] > tol):
     scores = []  
     this_best = last_score
     t0 = time.clock()
     for f in range(n_features):      
-      if f % 100 == 0: print "Testing Feature %d of %d - %d%%" % (f, n_features, f * 100 / n_features)
+      if f > 0 and f % 100 == 0: print "Testing Feature [%s] %d of %d - %d%%" % (column_names[f], f, n_features, f * 100 / n_features)
       if f not in good_features:      
         feats = list(good_features) + [f]
         Xt = X_train[:, feats]
@@ -23,23 +23,24 @@ def feature_select(X_train, y_train, model, n_samples=3500, n_iter=3,
         score, sem = do_cv(model, Xt, y_train, n_samples=n_samples, n_iter=n_iter, scoring=scoring, quiet=True)
         scores.append((score, f))            
         if (score - last_score > 0.1): 
-          print "> 10 percent improvement with feature, auto adding. %s -> %s" % (last_score, score)
+          print "> 10 percent improvement with feature %d [%s], auto adding. %s -> %s" % (f, column_names[f], last_score, score)
           break
         if (score > this_best): 
           this_best = score
-          print "Feature: %i Mean: %f (+/-%.2f) Diff: %.5f" % (f, score, sem, score - last_score)
+          print "Feature: %i [%s] Mean: %f (+/-%.2f) Diff: %.5f" % (f, column_names[f], score, sem, score - last_score)
     
     best = sorted(scores)[-1]  
     print "Last: %f Best: %s Feature: %s" % (last_score, best[0], best[1])
     last_score = best[0]
-    good_features.add(best[1])
+    good_features.append(best[1])
+    good_scores.append(last_score)
     score_hist.append(best)
-    sorted_good = sorted(list(good_features))
-    print "Iteration Took: %.2fm - Current Features: %s" % \
-      ((time.clock() - t0)/60, operator.itemgetter(sorted_good)(column_names))
+    print "Iteration Took: %.2fm - Current Features:\n%s\n[%s]" % \
+      ((time.clock() - t0)/60, good_features, operator.itemgetter(good_features)(column_names))
 
   # Remove last added feature from good_features
-  good_features.remove(score_hist[-1][1])
+  good_features.pop()
+  good_scores.pop()
   good_features = sorted(list(good_features))
-  print "Selected features %s - names %s" % (good_features, operator.itemgetter(good_features)(column_names))
+  print "Selected features [%s] - scores [%s] - names [%s]" % (good_features, good_scores, operator.itemgetter(good_features)(column_names))
   return good_features
--- vote_ensemble.py
+++ vote_ensemble.py
@@ -9,18 +9,31 @@ class VotingEnsemble(BaseEstimator, ClassifierMixin):
     self.voter = voter
 
   def fit(self, X, y):
-    for m in self.models: m.fit(X, y)
-
+    """X can either be a dataset or a list of datasets"""
+    if not(isinstance(X, list)): 
+      print 'X is a single dataset'
+      X = repeat(X, len(self.models))
+    for i, m in enumerate(self.models): m.fit(X[i], y)
     return self
 
   def predict(self, X):
-    all_preds = [m.predict(X) for m in self.models]
-    predictions = np.empty(len(X), dtype=type(all_preds[0][0]));
-    for i in range(len(X)):      
+    """X can either be a dataset or a list of datasets"""
+    if not(isinstance(X, list)): 
+      print 'X is a single dataset'
+      X = repeat(X, len(self.models))
+    all_preds = []
+    if self.voter == 'mean' || self.voter == 'median':  
+      all_preds = [m.predict_proba(X[i]) for m in enumerate(self.models)]
+      all_preds = map(lambda p:, float(p[0]) if isinstance(p, list) or isinstance(p, tuple) else float(p), all_preds)
+    else:
+      all_preds = [m.predict(X[i]) for i, m in enumerate(self.models)]
+
+    predictions = np.empty(len(X[0]), dtype=type(all_preds[0][0]));
+    for i in range(len(X[0])):      
       i_preds = [ps[i] for ps in all_preds]      
 
-      if (self.voter == 'majority'):                
-        maj = stats.mode(i_preds)[0]
-        predictions[i] = int(maj)
+      if (self.voter == 'majority'): predictions[i] = stats.mode(i_preds)[0]
+      elif (self.voter == 'mean'): predictions[i] = stats.mean(i_preds)[0]
+      elif (self.voter == 'median'): predictions[i] = stats.median(i_preds)[0]
       else: raise Error(self.voter + ' is not implemented')
     return predictions
