c577cf1e271a1544c03ceeca557fa471a8d0353e
Guido Tapia <guido.tapia@picnet.com.au>
2014-07-25 10:46:13 +1000

    Doing work on regression finder

--- regressor_finder.py
+++ regressor_finder.py
@@ -1,7 +1,9 @@
+import pandas as pd
+import numpy as np
+
 '''
 Regression Finder
 '''
-regressors = load_regressors_meta_()
 
 def find_optimal_regressor(X, y):
   scores = map(get_regressor_score_, regressors)
@@ -11,11 +13,22 @@ def find_optimal_regressor(X, y):
   return optimal
 
 def load_regressors_meta_():
+  regressors = pd.io.excel.read_excel('utils/sklearn algorithms.xlsx', 'regressors')
+  variables = pd.io.excel.read_excel('utils/sklearn algorithms.xlsx', 'variables')
+  results = []
+  current = None
+  for idx, r in regressors.iterrows():    
+    if isinstance(r.Algorithm, basestring):
+      if (current): results.append(current)
+      current = { 'name': r.Algorithm, 'args': [] }
+    elif isinstance(r.Args, basestring):      
+      current['args'].append({'name': r.Args, 'value': r.Values})
+  return results
 
 
 def get_regressor_score_(meta, X, y):
-  name = meta.name
-  args = parse_args_(meta.args)
+  name = meta['name']
+  args = parse_args_(meta['args'])
   clf = get_class_(name)
   gs = do_gs(clf, np.copy(X), y, args)
   gs.best_params_
@@ -25,11 +38,12 @@ def get_regressor_score_(meta, X, y):
 def parse_args_(meta_args):
   args = {}
   if not(meta_args): return args
-  for ma in meta_args: args[ma.name] = parse_arg_range_(ma.value)
+  for ma in meta_args: 
+    args[ma['name']] = parse_arg_range_(ma['value'])
   return args
 
 def parse_arg_range_(meta_value_str):
-
+  return []
 
 def get_class_(name):
   parts = name.split('.')
@@ -38,3 +52,7 @@ def get_class_(name):
   for comp in parts[1:]:
       m = getattr(m, comp)            
   return m
+
+
+regressors = load_regressors_meta_()
+print 'load_regressors_meta_: ', regressors
