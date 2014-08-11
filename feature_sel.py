6f5e25d785d4e78364a907ca1e9a6042be4b1de4
Guido Tapia <guido@tapia.com.au>
2014-08-11 10:17:47 +1000

    Made feature select multithreading compatible but has issues in windows

--- feature_sel.py
+++ feature_sel.py
@@ -9,18 +9,17 @@ from sklearn.externals import joblib
  # TODO 2): After each iteration we should do a test of all selected features
  #    to ensure they are all still required.  May be that the last feature
  #    added made another redundant.
-def feature_select(clf, X, y, n_samples=3500, n_iter=3, tol=0.00001, scoring=None, mandatory_columns=[]):
+def feature_select(clf, X, y, n_samples=3500, n_iter=3, tol=0.00001, scoring=None, mandatory_columns=[], n_jobs=1):
   if hasattr(clf, 'max_features') and clf.max_features: clf.max_features = None
 
   selected = map(lambda f: {'feature': f, 'score': 0, 'sem': 0}, mandatory_columns)
     
-  print "starting feature selected, features: ", X.shape[1]
+  print "starting feature selected, features: ", X.shape[1], "n_jobs:", n_jobs
 
   last_best = {'score': -1e6}
   while True:
-    t0 = time.time()
-    # iter_results = joblib.Parallel(n_jobs=6)(joblib.delayed(find_next_best_)(args))      
-    iter_results = find_next_best_(selected, clf, X, y, n_samples, n_iter, scoring)
+    t0 = time.time()    
+    iter_results = find_next_best_(selected, clf, X, y, n_samples, n_iter, scoring, n_jobs)
     this_best = max(iter_results, key=lambda s: s['score'])
     improvement = this_best['score'] - last_best['score']
     last_best = this_best
@@ -35,16 +34,18 @@ def feature_select(clf, X, y, n_samples=3500, n_iter=3, tol=0.00001, scoring=Non
   print "feature selection completed: ", selected
   return selected
     
-def find_next_best_(selected, clf, X, y, n_samples, n_iter, scoring):
-  results = []  
-  n_features = X.shape[1]
+def find_next_best_(selected, clf, X, y, n_samples, n_iter, scoring, n_jobs):
   selected_features = map(lambda s: s['feature'], selected)
-  for f in range(n_features):          
-    if f in selected_features: continue
+  to_test = filter(lambda f: f not in selected_features, range(X.shape[1]))
+  if n_jobs > 1:
+    return joblib.Parallel(n_jobs=6, max_nbytes=1e6, mmap_mode='r')(
+      joblib.delayed(get_feat_score_)(
+        selected_features, f, clf, X, y, n_samples, n_iter, scoring) for f in to_test)      
+  else:
+    return map(lambda f: get_feat_score_(selected_features, f, clf, X, y, n_samples, n_iter, scoring), to_test)
 
-    feats = list(selected_features) + [f]
-    Xt = X[:, feats]
-    score, sem = do_cv(clf, Xt, y, n_samples=n_samples, n_iter=n_iter, scoring=scoring, quiet=True)
-    results.append({'feature': f, 'score': score, 'sem': sem})                    
-  
-  return results
+def get_feat_score_(selected_features, f, clf, X, y, n_samples, n_iter, scoring):  
+  feats = list(selected_features) + [f]
+  Xt = X[:, feats]
+  score, sem = do_cv(clf, Xt, y, n_samples=n_samples, n_iter=n_iter, scoring=scoring, quiet=True)
+  return {'feature': f, 'score': score, 'sem': sem}
