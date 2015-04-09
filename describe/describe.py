6b0f4863598e00a8f28d677fc5a146bf3fb16196
Guido Tapia <guido@tapia.com.au>
2015-04-09 10:12:14 +1000

    Added reusable Bland-Altman plot util method

--- describe/describe.py
+++ describe/describe.py
@@ -273,6 +273,19 @@ class Describe():
   #       Utils
   #############################################################################
 
+  def _bland_altman_plot(data1, data2, *args, **kwargs):
+    data1     = np.asarray(data1)
+    data2     = np.asarray(data2)
+    mean      = np.mean([data1, data2], axis=0)
+    diff      = data1 - data2                   # Difference between data1 and data2
+    md        = np.mean(diff)                   # Mean of the difference
+    sd        = np.std(diff, axis=0)            # Standard deviation of the difference
+
+    plt.scatter(mean, diff, *args, **kwargs)
+    plt.axhline(md,        color='gray', linestyle='--')
+    plt.axhline(md + 2*sd, color='gray', linestyle='--')
+    plt.axhline(md - 2*sd, color='gray', linestyle='--')
+
   def _get_column_importances(self):
     start('_get_column_importances')
     rf = ensemble.RandomForestRegressor(50) if self.is_regression else ensemble.RandomForestClassifier(50)    
