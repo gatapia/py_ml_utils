53be357850609f3141eb8a05fba5cb8db76a2732
Guido Tapia <guido@tapia.com.au>
2015-04-10 16:37:34 +1000

    More work on data describe

--- describe/describe.py
+++ describe/describe.py
@@ -15,78 +15,76 @@ class Describe():
   def __init__(self):    
     self._importance_row_limit = 50000
     self._code_lines = []
-    self._text_lines = []        
+    self._text_lines = []      
+    self.cells = []  
 
   def show_classifier_performance(self, clf, X, y, use_proba=False):    
     predictions = X.self_predict_proba(clf, y) if use_proba else \
         X.self_predict(clf, y)
-    self.show_prediction_comparison(predictions, y)
+    self.show_prediction_comparison(predictions, y)        
 
-  def show_prediction_comparison(self, y1, y2):    
-    '''
-    TODO: implement (move things out of constructor)
-    '''
+  def show_prediction_comparison(self, y_true, y_pred):    
+    if y_true.shape != y_pred.shape:
+      raise Exception('y_true and y_pred are not compatible shapes (must be same rows and same columns)')
+    
+    self._create_notebook(self.get_prediction_comparison_cells(y_true, y_pred), False)    
 
-    # scatter plot
-    # bland altman plot
-    # confusion matrix
-    # correlations
-    # reliability diagram (see: http://fastml.com/classifier-calibration-with-platts-scaling-and-isotonic-regression/)
-    # predicted vs actual
-    if y1.shape != y2.shape: raise Exception('y1 and y2 are not compatible shapes (must be same rows and same columns)')
-    if y1.shape[1] > 1:
-      for col in y1.shape[1]:
-        # Heading for y index
-        self.show_prediction_comparison(y1[:, col], y2[:, col])
-    pass
+  def get_prediction_comparison_cells(self, y_true, y_pred):    
+    self.cells = []
+    start('get_prediction_comparison_cells')
+    dataset_name = '_get_prediction_comparison_cells_data'
+    dump(dataset_name, (y_true, y_pred)) 
+
+    self.y_true = y_true
+    self.y_pred = y_pred
+    self._do_global_imports(dataset_name, ['y_true', 'y_pred'])
+    self._do_target_description(self.y_true, False)
+    self._do_target_description(self.y_pred, False)
+    self._do_target_comparison_charts()    
+    stop('get_prediction_comparison_cells')    
+
 
   def show_dataset(self, X, opt_y=None):    
     '''
     TODO: If we are evaluating results over years or location then
       we should plot data by this same fold. Maybe ensure also that
       each year for each attribute correlates nicely
-    '''
-    self.columns = X.columns
-    self.cells = []
+    '''        
     self.original_rows = X.shape[0]
     Xy = X.subsample(opt_y, 5e5)  
     self.X = Xy if opt_y is None else Xy[0]
     self.X_no_nan = self.X.copy().missing('na', 0)
-    self.y = None if opt_y is None else Xy[1]
-    self.y_type = sklearn.utils.multiclass.type_of_target(self.y) if self.y is not None else 'na'
-    self.is_regression = self.y_type == 'continuous'
+    self.y = None if opt_y is None else Xy[1]    
+    self.is_regression = self._type(self.y) == 'continuous'
 
-
-    nb = nbf.new_notebook()
-    nb.cells = self.get_cells()
-    with open('dataset_description.ipynb', 'w') as f: 
-      f.write(nbf.writes(nb)) 
-
-    call(['ipython', 'notebook', 'dataset_description.ipynb'])
+    self._create_notebook(self.get_dataset_cells(), True)    
 
 
   def get_dataset_cells(self):
-    if len(self.cells) > 0: return list(self.cells)    
-    start('get_cells')
-    dump('_desc_data', (self.X, self.y)) 
+    self.cells = []
+    start('get_dataset_cells')
+    dataset_name = '_get_dataset_cells_data'
+    data_x = self.X.copy()
+    data_x['y'] = self.y
+    dump(dataset_name, (data_x, self.y)) 
 
-    self._do_global_imports()    
+    self._do_global_imports(dataset_name, ['X', 'y'])    
     self._do_header_markdown()    
     self._intialise_feature_scores()
     self._do_column_summary_table()
     # Do table of all columns x column with correlation values for
     #  all relationships
     self._do_column_summary_charts()
-    self._do_target_description()
+    if self.y is not None: self._do_target_description('y', True)
     self._do_all_columns_details()
-    stop('done get_cells')
+    stop('done get_dataset_cells')
     return list(self.cells)
   
   #############################################################################
   #       Header/Misc
   #############################################################################
 
-  def _do_global_imports(self):    
+  def _do_global_imports(self, dataset_name, variables):    
     imports = [
       '%matplotlib inline',
       'import numpy as np',
@@ -96,77 +94,95 @@ class Describe():
       'from sys import path',
       'path.append("utils")',
       'from misc import *',
+      'from graphs import *',
       'from pandas_extensions import *',
       'from sklearn import *',
       '\npd.set_option("display.notebook_repr_html", False)',
       'pd.set_option("display.max_columns", 20)',
       'pd.set_option("display.max_rows", 25)',
       'pylab.rcParams[\'figure.figsize\'] = (10.0, 6.0)',
-      'X, y = load("_desc_data")',
-      'X["_tmpy"] = y'
+      ','.join(variables) + ' = load("' + dataset_name + '")',
+      'MARKERS=[".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "*", "h", "H", "+", "x", "D", "d", "|", "_"]',
+      'COLOURS=["Blue","Green","Red","Cyan","Magenta","Yellow","Black","White"]',
     ]
-    self._code('\n'.join(imports), True)
+    self._code(imports, True)
 
   def _do_header_markdown(self):
     self._txt('<hr/>\n# General Description of Data')
-    self._name_value('Number of columns', len(self.columns))
+    self._name_value('Number of columns', len(self.X.columns))
     self._name_value('Number of rows', self.X.shape[0])
     if self.X.shape[0] > 5e5: self._name_value('Using subsample of', '50,000 rows')
     self._flush_cell()
 
   def _intialise_feature_scores(self):
     self.importances = self._get_column_importances()    
+    self.variances = self.X.var().values
     if self.is_regression:
       self.f_scores = self._get_column_f_regression_scores()    
-      self.col_details = zip(self.columns, self.importances, self.f_scores)
+      self.col_details = zip(self.X.columns, self.importances, self.f_scores, self.variances)
     else:
       self.f_classif_scores = self._get_column_f_classification_scores()    
       self.chi2_scores = self._get_column_chi2_classification_scores()          
-      self.col_details = zip(self.columns, self.importances, self.f_classif_scores, self.chi2_scores)
+      self.col_details = zip(self.X.columns, self.importances, self.f_classif_scores, self.chi2_scores, self.variances)
 
     self.col_details = sorted(self.col_details, key=lambda d: d[1], reverse=True)
       
   #############################################################################
-  #       Target Variabe
+  #       Target Variable
   #############################################################################
 
-  def _do_target_description(self):
-    if self.y is None or len(self.y) == 0: 
-      self._txt('<hr/>\n## No target variable specified\n\n\n', True)
-      return
-
-    uniques = pd.unique(self.y)
+  def _do_target_description(self, target_var, show_pca_graphs):
+    target_type = self._type(target_var)
+    uniques = pd.unique(target_var)
 
     self._txt('\n\n<hr/>\n## Target variable')
     
-    self._name_value('Inferred type', self.y_type)
+    self._name_value('Inferred type', target_type)
     self._name_value('Distinct values', len(uniques), True)
 
-    if self.y_type == 'continuous': self._continuous_charts(self.y)
-    elif self.y_type == 'binary' or self.y_type == 'multiclass': 
-      self._categorical_charts(self.y)
-      
-      # TODO: Consider doing a scatter plot scatter plot matrix, see
-      #    code here: https://github.com/breze-no-salt/breze/blob/master/
-      #         breze/learn/display.py
-      self._code([
-        'X2 = X.copy().missing("na", 0)',
-        'X2 = pd.DataFrame(decomposition.PCA(2).fit_transform(X2), columns=["A", "B"])',
-        'X2["y"] = y',
-        'markers=[".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "*", "h", "H", "+", "x", "D", "d", "|", "_"]',
-        'colours=["Blue","Green","Red","Cyan","Magenta","Yellow","Black","White"]',
-        'ax = None',
-        'for idx, v in enumerate(pd.unique(y)):',
-        '  col=colours[idx % len(colours)]',
-        '  mkr=markers[idx % len(markers)]',
-        '  X3 = X2[X2.y == v]'
-        '  if ax is None: ax = X3.plot(kind="scatter", x="A", y="B", label=v, alpha=0.2, marker=mkr, color=col)',
-        '  else: X3.plot(kind="scatter", x="A", y="B", label=v, ax=ax, alpha=0.2, marker=mkr, color=col)',
-        '_ = ax.set_title("2D PCA - Target Variable Distribution")'
-      ])
+    if target_type == 'continuous': self._continuous_charts(target_var)
+    elif target_type == 'binary' or target_type == 'multiclass': 
+      self._categorical_charts(target_var)
+      if show_pca_graphs:
+        self._code([
+          'X2 = X.copy().missing("na", 0)',        
+          'X2 = pd.DataFrame(decomposition.PCA(2).fit_transform(X2), columns=["A", "B"])',        
+          'X2["y"] = y',
+          'ax = None',
+          'for idx, v in enumerate(pd.unique(y)):',
+          '  col=COLOURS[idx % len(COLOURS)]',
+          '  mkr=MARKERS[idx % len(MARKERS)]',
+          '  X3 = X2[X2.y == v]',
+          '  if ax is None: ax = X3.plot(kind="scatter", x="A", y="B", label=v, alpha=0.2, marker=mkr, color=col)',
+          '  else: X3.plot(kind="scatter", x="A", y="B", label=v, ax=ax, alpha=0.2, marker=mkr, color=col)',
+          '_ = ax.set_title("2D PCA - Target Variable Distribution")'
+        ])
 
     self._flush_cell()
     self._txt('\n\n\n', True)
+
+
+  def _do_target_comparison_charts(self):
+    y_true_type = self._type(self.y_true)
+    y_pred_type = self._type(self.y_pred)
+    if y_true_type == 'continuous' and y_pred_type == 'continuous':
+      self._name_value('Pearson Cov:', self.y_true.cov(y_pred, method="pearson"))      
+      self._name_value('Spearman Cov:', self.y_true.cov(y_pred, method="spearman"), True)      
+      self._code('plt.scatter(y_true, y_pred, c=COLOURS, alpha=0.5).set_title("y_true vs y_pred")')
+      self._code('bland_altman_plot(y_true, y_pred)')
+    if y_true_type == 'multiclass' and y_pred_type == 'continuous': 
+      # using predict proba      
+      self._code('matrix = metrics.confusion_matrix(y_true, y_pred > 0.5)')
+      self._code('plot_confusion_matrix(matrix)')
+      self._code('reliability_curve(y_true, y_pred)')
+    elif y_true_type == 'multiclass' and y_pred_type == 'multiclass':
+      self._code('matrix = metrics.confusion_matrix(y_true, y_pred)')
+      self._code('plot_confusion_matrix(matrix)')
+      self._code('reliability_curve(y_true, y_pred)')
+
+    self._flush_cell()
+    self._txt('\n\n\n', True)
+
 
   #############################################################################
   #       Columns Summary
@@ -180,25 +196,26 @@ class Describe():
     self._txt('</table>', True)
 
   def _do_column_summary_header_row(self):
-    # TODO: Add a measure of variance of the column (zero variance is useless)
     cols = ['Column', 'Inferred', 'Specified', 'RF Imp Rank', 'RF Importance V', 'F Score']
     if not self.is_regression: cols.append('Chi2')
+    cols.append('Var')
     self._txt('<tr><th>' + '</th><th>'.join(cols) + '</th></tr>')
 
-  def _do_column_summary_row(self, idx, col):
-    col_name = col[0]
-    inferred = sklearn.utils.multiclass.type_of_target(self.X[col_name])
-    specified = self._get_column_specified_type(col_name)
+  def _do_column_summary_row(self, idx, col_details):
+    col_name = col_details[0]
+    inferred = self._type(self.X[col_name])
+    specified = self.  _get_column_specified_type(col_name)
     inf_same_specified = inferred == specified
     cols = [
       col_name, 
       inferred, 
       specified if inf_same_specified else self._warn(specified),
       idx + 1,
-      col[1]
+      col_details[1]      
     ]
-    if self.is_regression: cols.append(col[2])
-    else: cols += [col[2], col[3]]
+    if self.is_regression: cols.append(col_details[2])
+    else: cols += [col_details[2], col_details[3]]
+    cols.append(col_details[-1])
     self._txt('<tr><td>' + '</td><td>'.join(map(self._pretty, cols)) + '</td></tr>')
 
   def _do_column_summary_charts(self):
@@ -206,7 +223,12 @@ class Describe():
     top_5 = map(lambda c: c[0], self.col_details[:5])
     self._code('top_5 = ' + str(top_5))
     self._code('_ = pd.tools.plotting.scatter_matrix(X[top_5], alpha=0.2, figsize=(12, 12), diagonal="kde")', True)
-    # TODO: Add PCA scree plot
+
+    self._txt('<hr/>\n#PCA Explained Variable Ratios', True)
+    self._code(['X2 = X.copy().missing("na", 0)',
+        'ratios = decomposition.PCA(X2.shape[1]).fit(X2).explained_variance_ratio_',
+        'df = pd.DataFrame({"variance": ratios})',
+        '_ = df.plot(kind="bar", figsize=(12, 10))'], True)
 
 
   #############################################################################
@@ -222,14 +244,14 @@ class Describe():
   def _do_column_details(self, idx, col):    
     col_name = col[0]
     c = self.X[col_name]    
-    specified_type = self._get_column_specified_type(col_name)
-    inferred_type = sklearn.utils.multiclass.type_of_target(c)
+    specifiedself._type = self.  _get_column_specified_type(col_name)
+    inferredself._type = self._type(c)
 
     self._txt('\n\n<hr/>\n## ' + col_name)
 
     self._name_value('Distinct values', len(pd.unique(c)))
-    self._name_value('Specified type', self._get_column_specified_type(col_name))
-    self._name_value('Inferred type', sklearn.utils.multiclass.type_of_target(c))
+    self._name_value('Specified type', self.  _get_column_specified_type(col_name))
+    self._name_value('Inferred type', self._type(c))
     self._name_value('RF Importance Rank', idx + 1)
     self._name_value('RF Importance Score', col[1])
 
@@ -237,8 +259,9 @@ class Describe():
     else:
       self._name_value('F-Score', col[2])
       self._name_value('Chi2 Score', col[3])
+    self._name_value('Variance', col[-1])
 
-    if specified_type != inferred_type:
+    if specifiedself._type != inferredself._type:
       self._txt(self._warn('Note:') + ' Check if specified type is correct as it does not match inferred type')
     
     self._txt('\n', True)
@@ -256,7 +279,7 @@ class Describe():
     self._txt('\n\n\n', True)
 
   def _categorical_charts(self, series):
-    identifier = '_ = X["_tmpy"]' if series is self.y else '_ = X["' + series.name + '"]'
+    identifier = '_ = X["y"]' if series is self.y else '_ = X["' + series.name + '"]'
     self._code('fig, axs = plt.subplots(1,1)')
     self._code(identifier + '.value_counts().plot(kind="barh")')        
     self._code('axs.set_ylabel("Value")')
@@ -264,19 +287,24 @@ class Describe():
     self._code('_ = axs.set_title("Field Values")')
 
   def _continuous_charts(self, series):
-    identifier = '_ = X["_tmpy"]' if series is self.y else '_ = X["' + series.name + '"]'    
-    self._code('fig, axs = plt.subplots(1,2)')
-    # TODO: Show boxcox transformed distribution also???    
-    self._code(identifier + '.hist(bins=20, ax=axs[0])')
-    self._code(identifier + '.plot(kind="box", ax=axs[1])')
+    identifier = 'X["y"]' if series is self.y else 'X["' + series.name + '"]'
+    self._code([
+      'fig, axs = plt.subplots(1,3, figsize=(12, 8))',
+      identifier + '.hist(bins=30, ax=axs[0]).set_title("Distribution")',
+      'bc = scipy.stats.boxcox(' + identifier +')',
+      'df = pd.DataFrame(bc[0])',
+      'df.hist(bins=30, ax=axs[1])',
+      'axs[1].set_title("Boxcox Distribution l=%.3f" % bc[1])',
+      '_ = ' + identifier + '.plot(kind="box", ax=axs[2]).set_title("Boxplot")'
+    ])
 
   def _relationship(self, a, b):
     identifier_a = 'X["' + a.name + '"]'    
-    identifier_b = 'X["_tmpy"]' if b is self.y else 'X["' + b.name + '"]'
-    identifier_b2 = '_tmpy' if b is self.y else b.name
+    identifier_b = 'X["y"]' if b is self.y else 'X["' + b.name + '"]'
+    identifier_b2 = 'y' if b is self.y else b.name
 
-    type_a = sklearn.utils.multiclass.type_of_target(a)
-    type_b = sklearn.utils.multiclass.type_of_target(b)
+    type_a = self._type(a)
+    type_b = self._type(b)
 
     if type_a == 'continuous':
       if type_b == 'continuous' :
@@ -288,7 +316,7 @@ class Describe():
         self._code('fig, axs = plt.subplots(1,2)')         
         self._code('_ = X[["' + a.name + '", "' + identifier_b2 + '"]].boxplot(by="' + identifier_b2 + '", ax=axs[0])')
         self._code('_ = X.plot(kind="scatter", x="' + identifier_b2 + '", y="' + a.name +'", ax=axs[1])')      
-        self._code('_ = X.hist(column="' + a.name + '", by="_tmpy", figsize=(12, 12))')
+        self._code('_ = X.hist(column="' + a.name + '", by="y", figsize=(12, 12))')
 
 
     if type_a == 'multiclass':
@@ -309,19 +337,6 @@ class Describe():
   #############################################################################
   #       Utils
   #############################################################################
-
-  def _bland_altman_plot(data1, data2, *args, **kwargs):
-    data1     = np.asarray(data1)
-    data2     = np.asarray(data2)
-    mean      = np.mean([data1, data2], axis=0)
-    diff      = data1 - data2                   # Difference between data1 and data2
-    md        = np.mean(diff)                   # Mean of the difference
-    sd        = np.std(diff, axis=0)            # Standard deviation of the difference
-
-    plt.scatter(mean, diff, *args, **kwargs)
-    plt.axhline(md,        color='gray', linestyle='--')
-    plt.axhline(md + 2*sd, color='gray', linestyle='--')
-    plt.axhline(md - 2*sd, color='gray', linestyle='--')
 
   def _get_column_importances(self):
     start('_get_column_importances')
@@ -348,7 +363,7 @@ class Describe():
     stop('_get_column_f_classification_scores')
     return scores
 
-  def _get_column_specified_type(self, col_name):
+  def   _get_column_specified_type(self, col_name):
     if col_name.startswith('c_'): return 'categorical'
     if col_name.startswith('i_'): return 'categorical index'
     if col_name.startswith('n_'): return 'continuous'
@@ -392,3 +407,15 @@ class Describe():
 
     self._code_lines = []
     self._text_lines = []
+
+  def _type(self, data):
+    if data is None: return None
+    return sklearn.utils.multiclass.type_of_target(data)
+
+  def _create_notebook(cells, start_notebook):
+    nb = nbf.new_notebook()
+    nb.cells = cells
+    with open('dataset_description.ipynb', 'w') as f: 
+      f.write(nbf.writes(nb)) 
+
+    if start_notebook:call(['ipython', 'notebook', 'dataset_description.ipynb'])
---
+++ graphs.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bland_altman_plot(data1, data2, *args, **kwargs):
+  data1   = np.asarray(data1)
+  data2   = np.asarray(data2)
+  mean    = np.mean([data1, data2], axis=0)
+  diff    = data1 - data2            # Difference between data1 and data2
+  md    = np.mean(diff)              # Mean of the difference
+  sd    = np.std(diff, axis=0)       # Standard deviation of the difference
+
+  plt.scatter(mean, diff, *args, **kwargs)
+  plt.axhline(md,    color='gray', linestyle='--')
+  plt.axhline(md + 2*sd, color='gray', linestyle='--')
+  plt.axhline(md - 2*sd, color='gray', linestyle='--')
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=None):
+  if labels is None: labels = range(cm.shape[0])
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(cm.shape[0])
+  plt.xticks(tick_marks, labels, rotation=45)
+  plt.yticks(tick_marks, labels)
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')  
+
+def reliability_curve(y_true, y_score, bins=10, normalize=False):
+  """Compute reliability curve
+
+  Reliability curves allow checking if the predicted probabilities of a
+  binary classifier are well calibrated. This function returns two arrays
+  which encode a mapping from predicted probability to empirical probability.
+  For this, the predicted probabilities are partitioned into equally sized
+  bins and the mean predicted probability and the mean empirical probabilties
+  in the bins are computed. For perfectly calibrated predictions, both
+  quantities whould be approximately equal (for sufficiently many test
+  samples).
+
+  Note: this implementation is restricted to binary classification.
+
+  Parameters
+  ----------
+
+  y_true : array, shape = [n_samples]
+      True binary labels (0 or 1).
+
+  y_score : array, shape = [n_samples]
+      Target scores, can either be probability estimates of the positive
+      class or confidence values. If normalize is False, y_score must be in
+      the interval [0, 1]
+
+  bins : int, optional, default=10
+      The number of bins into which the y_scores are partitioned.
+      Note: n_samples should be considerably larger than bins such that
+            there is sufficient data in each bin to get a reliable estimate
+            of the reliability
+
+  normalize : bool, optional, default=False
+      Whether y_score needs to be normalized into the bin [0, 1]. If True,
+      the smallest value in y_score is mapped onto 0 and the largest one
+      onto 1.
+
+
+  Returns
+  -------
+  y_score_bin_mean : array, shape = [bins]
+      The mean predicted y_score in the respective bins.
+
+  empirical_prob_pos : array, shape = [bins]
+      The empirical probability (frequency) of the positive class (+1) in the
+      respective bins.
+
+
+  References
+  ----------
+  .. [1] `Predicting Good Probabilities with Supervised Learning
+          <http://machinelearning.wustl.edu/mlpapers/paper_files/icml2005_Niculescu-MizilC05.pdf>`_
+
+  """
+  if normalize:  # Normalize scores into bin [0, 1]
+    y_score = (y_score - y_score.min()) / (y_score.max() - y_score.min())
+
+  bin_width = 1.0 / bins
+  bin_centers = np.linspace(0, 1.0 - bin_width, bins) + bin_width / 2
+
+  y_score_bin_mean = np.empty(bins)
+  empirical_prob_pos = np.empty(bins)
+  for i, threshold in enumerate(bin_centers):
+    # determine all samples where y_score falls into the i-th bin
+    bin_idx = np.logical_and(threshold - bin_width / 2 < y_score,
+                             y_score <= threshold + bin_width / 2)
+    # Store mean y_score and mean empirical probability of positive class
+    y_score_bin_mean[i] = y_score[bin_idx].mean()
+    empirical_prob_pos[i] = y_true[bin_idx].mean()
+  return y_score_bin_mean, empirical_prob_pos
