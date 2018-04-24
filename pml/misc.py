3c31b88a8e91cf41b67b785575c9e89dff12e970
Guido Tapia <guido.tapia@picnet.com.au>
2018-04-24 15:36:45 +1000

    added pml.progress to show notebook progress

--- pml/misc.py
+++ pml/misc.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, absolute_import
 
-import sys, gzip, time, datetime, random, os, logging, gc, \
-
+import sys, gzip, time, datetime, random, os, logging, gc,\
     scipy, sklearn, sklearn.model_selection,\
     sklearn.utils, sklearn.externals.joblib, inspect, bcolz, pickle
 import numpy as np
@@ -303,6 +302,63 @@ def self_chunked_op(X, y, op, cv=5):
     chunks = predictions if chunks is None else np.concatenate((chunks, predictions))
   df = pd.DataFrame(data=chunks, index=indexes)
   return df.sort()
+
+def progress(sequence, every=None, size=None, name='Items'):
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)     # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{name}: {index} / ?'.format(
+                        name=name,
+                        index=index
+                    )
+                else:
+                    progress.value = index
+                    label.value = u'{name}: {index} / {size}'.format(
+                        name=name,
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = "{name}: {index}".format(
+            name=name,
+            index=str(index or '?')
+        )
+
 
 def dbg(*args):
   if cfg['debug']: print(*args)
